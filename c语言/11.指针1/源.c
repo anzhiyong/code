468a495a1e03857bd0454be5ddf2b58f901c0a5d
an <2056048952@qq.com>
2024-03-26 21:52:21 +0800

    指针

---
+++ c语言/11.指针1/源.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void test1()
+//{
+//	int n = 10;
+//	int m = 20;
+//	int *const p = &n;
+//	*p = 20;
+//	p = &m;
+//
+//}
+//int main()
+//{
+//	test1();
+//
+//
+//}
+
+//int main()
+//{
+//	int arr[] = { 34,325,23,3,53,22,4,4,34,3,4,43,4,3 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = &arr[0];
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//}
+
