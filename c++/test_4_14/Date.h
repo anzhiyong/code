f2b245932fc93eda775598030446696f27fd4734
an <2056048952@qq.com>
2024-04-15 23:44:48 +0800

    4——15

--- c++/test_4_14/Date.h
+++ c++/test_4_14/Date.h
@@ -6,6 +6,10 @@ using namespace std;
 #include<assert.h>
 class Date
 {
+
+	// 友元函数声明
+	friend ostream& operator<<(ostream& out, const Date& d);
+	friend istream& operator>>(istream& in, Date& d);
 public:
 	Date(int year = 1900, int month = 1, int day = 1);
 	void Print()const;
@@ -35,11 +39,11 @@ public:
 
 	//d1 +=100;
 	Date& operator+=(int day);
-	Date operator+=(int day) const;
+	Date operator+(int day);
 	//d1 -=100;
 	Date& operator-=(int day);
 	//d1-100;
-	Date operator-(int day);
+	Date operator-(int day) const;
 	//d1-d2
 	int operator-(const Date& d) const;
 
@@ -48,7 +52,7 @@ public:
 	Date operator++(int);
 
 	Date& operator--();
-	Date operator--() const;
+	Date operator--(int);
 
 private:
 	int _year;
@@ -58,3 +62,7 @@ private:
 
 
 };
+
+//重载
+ostream& operator<<(ostream& out, const Date& d);
+istream& operator>>(istream& in, Date& d);
--- c++/test_4_14/Test.cpp
+++ c++/test_4_14/Test.cpp
@@ -3,8 +3,11 @@
 int main()
 {
 	Date d1(2003,5,14);
-	Date d2(2024,4,15);
+	Date d2 = d1 + 90;
+	int a = d2 - d1;
+	d1--;
 	d1.Print();
 	d2.Print();
+	cout << a << endl;
 	return 0;
 }
