f0286d74a5047d1c5c63696b215c9d864604a5c0
an <2056048952@qq.com>
2024-05-29 19:17:24 +0800

    栈和队列的模拟实现

--- c++/stack_queue/Test.cpp
+++ c++/stack_queue/Test.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+using namespace std;
 #include<stack>
 #include<queue>
-using namespace std;
 
-int main()
+
+#include"stack_queue.h"
+
+void test_stack1()
 {
 	stack<int> s;
 	s.push(1);
@@ -18,6 +21,58 @@ int main()
 		//删除栈元素
 		s.pop();
 	}
+}
 
+//void test_stack2()
+//{
+//	an::stack<int, vector<int>> s;
+//	s.push(1);
+//	s.push(2);
+//	s.push(3);
+//	s.push(4);
+//	s.push(5);
+//	
+//	while (!s.empty())  //栈不为空继续
+//	{
+//		//获取栈顶元素
+//		cout << s.top() << endl;
+//		//删除栈元素
+//		s.pop();
+//	}
+//}
+
+void test_queue1()
+{
+	queue<int> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	
+	while (!q.empty())
+	{
+		cout << q.back();
+		q.pop();
+	}
+}
+
+void test_queue2()
+{
+	an::queue<int, vector<int>> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+
+	while (!q.empty())
+	{
+		cout << q.front();
+		q.pop();
+	}
+}
+int main()
+{
+	
+	test_queue2();
 	return 0;
 }
---
+++ c++/stack_queue/stack_queue.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+#include<vector>
+#include<list>
+
+namespace an
+{
+	
+	template<class T, class Container>
+	class stack
+	{
+	public:
+		void push(const T& x)
+		{
+			_con.push_back(x);
+		}
+
+		void pop()
+		{
+			_con.pop_back();
+		}
+
+		const T& top()
+		{
+			return _con.back();
+		}
+
+		bool empty()
+		{
+			return _con.empty();
+		}
+
+		size_t size()
+		{
+			_con.size();
+		}
+	private:
+		Container _con;
+	};
+
+	template <class T, class C>
+	class queue
+	{
+	private :
+		C _c;
+	public:
+		void push(const T& x)
+		{
+			_c.push_back(x);
+		}
+
+		void pop()
+		{
+
+			_c.erase(_c.begin());
+		}
+
+		T& back()
+		{
+			return _c.back();
+		}
+		
+		T& front()
+		{
+			return _c.front();
+		}
+		bool empty()
+		{
+			return _c.empty();
+		}
+
+		size_t size()
+		{
+			_c.size();
+		}
+
+		
+	};
+}
