9fb9f6913e15a44453fb6731fed005d58a88feb9
an <2056048952@qq.com>
2024-11-01 20:08:41 +0800

    课堂代码

---
+++ Linux/lesson6/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int main()
+{
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    printf("hello bit, hello : %d\n", MAX);
+    return 0;
+}
