fdd9a11e181edee2a24ca774876b7c251ad58887
Zhiyong <anzhiyong2003@gmail.com>
2025-08-17 20:32:03 +0800

    坐标系

--- Qt/Position/widget.cpp
+++ Qt/Position/widget.cpp
@@ -1,11 +1,16 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPushButton>
+
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    QPushButton* button = new QPushButton(this);
+    button->setText("按钮");
+    button->move(200, 300);
 }
 
 Widget::~Widget()
